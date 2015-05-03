c99b48794070a64f2c67673ba5362d1afe70c39a
Leandro Barbosa <leandrobar93@gmail.com>
2015-05-03 16:28:50 -0300

    adicionando broker no settings.py

--- desafiosimbiose/desafiosimbiose/settings.py
+++ desafiosimbiose/desafiosimbiose/settings.py
@@ -100,3 +100,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+BROKER_URL = "amqp://simbiosevetures:qwert741258@locahost:5672/simbiosevetureshost"
